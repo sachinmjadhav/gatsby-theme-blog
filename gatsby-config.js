6210b463453651328f10ddacf04f2d9bb008991c
sachinmjadhav <sachin0321@gmail.com>
2019-10-28 00:58:22 +0530

    removed gatsby-plugin-mdx from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,26 +12,6 @@ module.exports = ({ contentPath="posts", basePath="/"  }) => ({
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        typeName: "Post",
-        extensions: [`.mdx`, `.md`],
-        deaultLayouts: {
-          default: require.resolve('./src/templates/post.js')
-        },
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1035,
-              backgroundColor: "none"
-            }
-          }
-        ],
-        plugins: [`gatsby-remark-images`]
-      }
-    },
-    {
       resolve: `gatsby-transformer-remark`
     }
   ]
